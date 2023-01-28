7d03a687ad62f27dcce8d55b87cc0ed8ebbef782
Fraser <fraser.russell1991@gmail.com>
2023-01-28 15:02:32 +0000

    Fade functionality added to about me

--- portfolio/src/components/AboutMe.js
+++ portfolio/src/components/AboutMe.js
@@ -17,10 +17,11 @@ const Paragraph = styled.p`
 const AboutMe = () => {
     return (
         <AboutMeBox className="About-me" id="AboutMe">
-        <Fade left-duration={1000}>
+        <Fade left>
         <Title className="About-me-title">Hi, I'm Fraser</Title>
         </Fade>
-        <Fade right-duration={1000}>
+        {/* <Fade right-duration={2000}> */}
+        <Fade right>
         <Paragraph>I'm new to the tech field. I recently invested into a CodeClan course to expand my understanding of the field. In my time at CodeClan I've explored a variety of languages and used those to create projects. After my time at CodeClan I hope to explore a wide range of topics before going deeper into a more specific topic.</Paragraph>
         </Fade>
         </AboutMeBox>
